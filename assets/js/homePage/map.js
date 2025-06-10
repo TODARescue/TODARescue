e63a2008257bea868ad095726322d789c7e70b84
KARMken <ken.aeron.milorin@gmail.com>
2025-06-11 04:13:22 +0800

    #2 Fixed the full screen of map, #62 Added driver info card, #65 Implement Custom Marker

--- assets/js/homePage/map.js
+++ assets/js/homePage/map.js
@@ -19,45 +19,64 @@ const profileIcon = L.icon({
   className: "rounded-icon",
 });
 
-// HTML content for the popup [image with name overlay]
+// HTML popup content
 const popupContent = `
-        <div class="profile-popup" style="position: relative; text-align: center;">
-            <div class="name-overlay" style="position: absolute; bottom: 6px; left: 50%; transform: translateX(-50%); color: white; font-weight: bold; font-size: 12px; text-shadow: 0 0 3px black;">
-                You 
-            </div>
-            <img src="../assets/images/profile-default.png" style="width: 50px; height: 50px; border-radius: 50%; border: 2px solid white;" />
+    <div class="profile-popup" style="position: relative; text-align: center;">
+        <div class="name-overlay" style="position: absolute; bottom: 6px; left: 50%; transform: translateX(-50%); color: white; font-weight: bold; font-size: 12px; text-shadow: 0 0 3px black;">
+            You 
         </div>
-    `;
+        <img src="../assets/images/profile-default.png" style="width: 50px; height: 50px; border-radius: 50%; border: 2px solid white;" />
+    </div>
+`;
 
-// Lalagyan function after magawa sa settings yung share location option
-// setTimeout(() => {
+// Variables to store current marker and circles
+let currentMarker = null;
+let accuracyCircle = null;
+let accuracyOutline = null;
+
+// Start locating
 map.locate({
   setView: true,
   maxZoom: 19,
-  watch: true,
+  watch: false,
   enableHighAccuracy: true,
 });
-// }, 3000);
 
 // Location found
 map.on("locationfound", function (e) {
-  L.marker(e.latlng, {
+  map.setView(e.latlng, 19);
+
+  // Remove previous marker and circles if they exist
+  if (currentMarker) {
+    map.removeLayer(currentMarker);
+  }
+  if (accuracyCircle) {
+    map.removeLayer(accuracyCircle);
+  }
+  if (accuracyOutline) {
+    map.removeLayer(accuracyOutline);
+  }
+
+  // Add new marker
+  currentMarker = L.marker(e.latlng, {
     icon: profileIcon,
   })
     .addTo(map)
-    .bindPopup(popupContent);
+    .bindPopup(popupContent)
+    .on("click", function () {
+      map.setView(currentMarker.getLatLng(), 19);
+    });
 
-  // Accuracy glow
-  L.circle(e.latlng, {
-    radius: e.accuracy + 20,
+  // Add new accuracy circles
+  accuracyOutline = L.circle(e.latlng, {
+    radius: e.accuracy + 10,
     color: "#2ebcbc",
     weight: 6,
     opacity: 0.1,
     fillOpacity: 0,
   }).addTo(map);
 
-  // Inner accuracy circle
-  L.circle(e.latlng, {
+  accuracyCircle = L.circle(e.latlng, {
     radius: e.accuracy,
     color: "#2ebcbc",
     weight: 2,
@@ -71,9 +90,17 @@ map.on("locationerror", function () {
   alert("Could not detect location. Showing default location.");
 
   map.setView(fallbackCoords, 19);
-  L.marker(fallbackCoords, {
+
+  if (currentMarker) map.removeLayer(currentMarker);
+  if (accuracyCircle) map.removeLayer(accuracyCircle);
+  if (accuracyOutline) map.removeLayer(accuracyOutline);
+
+  currentMarker = L.marker(fallbackCoords, {
     icon: profileIcon,
   })
     .addTo(map)
-    .bindPopup(popupContent);
+    .bindPopup(popupContent)
+    .on("click", function () {
+      map.setView(currentMarker.getLatLng(), 19);
+    });
 });
