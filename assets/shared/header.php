c66b6f4808ad8794ff5d609a518e0030b41ddfd0
jsnotprom <johnstephengalarrita@gmail.com>
2025-06-12 23:23:32 +0800

    WIP

--- assets/shared/header.php
+++ assets/shared/header.php
@@ -1,22 +1,9 @@
-<style>
-    body {
-        font-family: 'Inter', san serif;
-    }
 
-    .header {
-        box-shadow:
-            0 -1px 6px 3px rgba(0, 0, 0, 0.1),
-            0 0 18px 6px rgba(0, 0, 0, 0.15);
-    }
-</style>
-
-<body>
-   <div class="container-fluid position-fixed top-0 start-0 end-0 bg-white shadow rounded-5" style="z-index: 1030;">
+   <div class="container-fluid position-fixed top-0 start-0 end-0 bg-white shadow rounded-bottom-5" style="z-index: 1030;">
     <div class="row">
-        <div class="col d-flex align-items-center p-2 rounded-bottom-4">
+        <div class="col d-flex align-items-center p-3 rounded-bottom-4">
             <img src="../assets/shared/navbar-icons/arrow-back.svg" alt="Back" class="img-fluid m-2" style="height: 40px;" />
             <h3 class="fw-bold m-0 ps-2">Emergency Contact</h3>
         </div>
     </div>
 </div>
-</body>
