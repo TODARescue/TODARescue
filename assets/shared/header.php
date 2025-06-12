069eb55e0e7a2be1bc5984bdec7700902be0ee62
jannerch <jannamaemacatangay5@gmail.com>
2025-06-12 21:46:49 +0800

    [ENHANCEMENT] The radius of the header is now updated #31

--- assets/shared/header.php
+++ assets/shared/header.php
@@ -11,7 +11,7 @@
 </style>
 
 <body>
-   <div class="container-fluid position-fixed top-0 start-0 end-0 bg-white shadow rounded-5" style="z-index: 1030;">
+   <div class="container-fluid position-fixed top-0 start-0 end-0 bg-white shadow rounded-bottom-5" style="z-index: 1030;">
     <div class="row">
         <div class="col d-flex align-items-center p-2 rounded-bottom-4">
             <img src="../assets/shared/navbar-icons/arrow-back.svg" alt="Back" class="img-fluid m-2" style="height: 40px;" />
