e05e0d50e1f50b786736e9b9953d1108fd239ff7
Edcel <edcelesquivel14@gmail.com>
2025-07-01 13:49:48 +0800

    Change the db name for todarescue

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -2,7 +2,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "mockuptoda";
+$db = "todarescue";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
